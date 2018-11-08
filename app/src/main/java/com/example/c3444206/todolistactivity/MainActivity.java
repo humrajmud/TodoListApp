69be4e7b3536ac19a39a3dd7e29f9910eca66ee2
humraj <humrajmudhar@hotmail.com>
2018-11-08 16:09:11 +0000

    initial setup

---
+++ app/src/main/java/com/example/c3444206/todolistactivity/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.c3444206.todolistactivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
